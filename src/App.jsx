2fcf27c45d8041e54987af45e04e345e2397df17
sanjudev1 <sanjeevch.131@gmail.com>
2023-03-20 14:25:15 +0530

    Key is ignored

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+require('dotenv').config();
 
 // const API_KEY = "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";
 // const API_KEY ="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";
-const API_KEY="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"
+const API_KEY=process.env.API_KEY
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
