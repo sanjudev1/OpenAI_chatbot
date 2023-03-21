38e92077606ce8cb7ec4cef3b4891a01997e88ba
sanjudev1 <sanjeevch.131@gmail.com>
2023-03-21 15:07:49 +0530

    vite_Key is ignored

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ const api_key=import.meta.env.VITE_SOME_KEY
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEYs =api_key
+const API_KEY =api_key
 
 
 // const API_KEY = "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";
 // const API_KEY ="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";
-const API_KEY=API_KEYs
+
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
